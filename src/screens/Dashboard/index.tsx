c3b61e5ae4c22ec3bb94e0d2ce784f93133d2edf
Milton <miltonsergiom@gmail.com>
2022-03-17 02:54:40 -0300

    Created Greetings header and scrollview cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,41 @@
 import React from "react";
+import { HighlightCard } from "../../components/HighlightCard";
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  User,
+  UserGreeting,
+  Photo,
+  UserName,
+  Icon,
+  HightlightCards,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: "https://github.com/miltonsmendes.png" }} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Milton</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+
+      <HightlightCards>
+        <HighlightCard />
+        <HighlightCard />
+        <HighlightCard />
+      </HightlightCards>
     </Container>
   );
 }
